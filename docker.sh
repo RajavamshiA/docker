dfc5568df3d5c27fd1af2b8f4e66204c4a7072e4
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-03-04 20:11:48 +0000

    Added Cluster debugging script into docker.sh file

--- docker.sh
+++ docker.sh
@@ -12,3 +12,19 @@ dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-co
 systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
+
+#Bash script for the waiter
+# Define the EKS cluster name
+CLUSTER_NAME="cluster-1"
+
+# Wait for the EKS cluster to become active
+echo "Waiting for EKS cluster '$CLUSTER_NAME' to become active..."
+aws eks wait cluster-active --name "$CLUSTER_NAME" --max-attempts 60
+
+# Check if the command was successful
+if [ $? -eq 0 ]; then
+    echo "EKS cluster '$CLUSTER_NAME' is now active!"
+else
+    echo "Failed: Waiter state transitioned to Failure."
+    exit 1
+fi
