65e9b562c44ce65dba4fa2503a2440ab0e222a0c
RajavamshiA <rajavamshiankam2019@gmail.com>
2025-08-09 12:37:09 +0530

    installing kubectl into docker installed server

--- docker.sh
+++ docker.sh
@@ -32,9 +32,16 @@ fi
 #kubectl installation
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.32.0/2024-12-20/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mv kubectl /usr/local/bin/kubectl
+mv kubectl /usr/local/bin/kubectl       
 
 #eksctl
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
 curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
 tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
 mv /tmp/eksctl /usr/local/bin
+
+#Install Helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
